b276477cb5ed58d2bcdfe627266bb3829b5306ea
Prabhakar <itzprabhakar8919@gmail.com>
2026-04-01 13:41:07 +0530

    Fixed project alignment issue

--- project_master/settings.py
+++ project_master/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,7 +53,7 @@ WSGI_APPLICATION = 'project_master.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project_management',
+        'NAME': 'project_db',
         'USER': 'root',
         'PASSWORD': 'prabhakar8919',
         'HOST': '127.0.0.1',
@@ -85,6 +86,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
